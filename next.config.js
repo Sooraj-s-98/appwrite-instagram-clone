e5fac4ae86591ec131753c4f340709523733ca53
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2021-10-16 19:06:34 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
   env: {
     REACT_APP_ENDPOINT: 'http://localhost:5000/v1', // Your Appwrite endpoint
     REACT_APP_PROJECT: '6162ae56f0f1f', // Your Appwrite project ID
