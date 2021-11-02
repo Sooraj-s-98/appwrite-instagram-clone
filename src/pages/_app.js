f7abc5d916be7c74d258b8d3d57ad32799a66a2a
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2021-11-02 08:48:50 +0530

    remove open

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,6 +16,7 @@ export default class MyApp extends App {
     return (
       <>
         <Head>
+          <title>Appwrite Instagram</title>
           <meta
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
